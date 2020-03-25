7aa3b7a7c2d77a2cccbf0bbf57cce402bba519c4
Luiz kof <luizkoff@gmail.com>
2020-03-25 16:02:44 -0300

    Updates

---
+++ precache-manifest.8bfff41a8dd12d93c6ca3bce52888289.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6306750f2e872ccf23582956e721b7c",
+    "url": "/monitorCOVID19/index.html"
+  },
+  {
+    "revision": "973865eaff63dc727030",
+    "url": "/monitorCOVID19/static/css/main.4665a683.chunk.css"
+  },
+  {
+    "revision": "cb7be80dc797e8d8d1e0",
+    "url": "/monitorCOVID19/static/js/2.ae8b7e66.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monitorCOVID19/static/js/2.ae8b7e66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "973865eaff63dc727030",
+    "url": "/monitorCOVID19/static/js/main.3476c5af.chunk.js"
+  },
+  {
+    "revision": "0d9dd1c15b7aa413dbcc",
+    "url": "/monitorCOVID19/static/js/runtime-main.4283d58b.js"
+  }
+]);
